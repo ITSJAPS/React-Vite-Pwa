3c0e894d7693c6d9d425f1712659d9ddaeeeff7f
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-10-12 18:42:10 -0600

    Logo de coca

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import { Outlet, Link } from 'react-router-dom';
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      
+      <a href="https://vitejs.dev" target="_blank">
+          <img src="../images/logoDragon.png" className="logo" alt="Vite logo" />
+        </a>
+
+      <h1>Titulo placoso JAPS</h1>
       <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
       <Link to="/expenses">Expenses</Link>|{' '}
       <Link to="/invoices">Invoices</Link> 
